42ebec39f3e277d43568ab09fafa7db32a2733c1
cyber <cyber>
2017-05-09 21:48:24 +0300

    syntax

--- src/Components/Sidebar/Menu/Submenu/index.js
+++ src/Components/Sidebar/Menu/Submenu/index.js
@@ -3,9 +3,15 @@ import React from 'react';
 const Submenu = () => {
   return (
     <nav className="menu__submenu">
-      <div className="menu__subitem"><a className="menu__link menu__link_active" href="#">Shoes</a></div>
-      <div className="menu__subitem"><a className="menu__link" href="#">Clothing</a></div>
-      <div className="menu__subitem"><a className="menu__link" href="#">Accesories</a></div>
+      <div className="menu__subitem">
+        <a className="menu__link menu__link_active" href="#">Shoes</a>
+      </div>
+      <div className="menu__subitem">
+        <a className="menu__link" href="#">Clothing</a>
+      </div>
+      <div className="menu__subitem">
+        <a className="menu__link" href="#">Accesories</a>
+      </div>
     </nav>
   )
 };
--- src/Components/Sidebar/Menu/index.js
+++ src/Components/Sidebar/Menu/index.js
@@ -4,11 +4,16 @@ import Submenu from './Submenu';
 const Menu = () => {
   return (
     <nav className="menu">
-      <div className="menu__item menu__item_selected"><span>Sports</span>
+      <div className="menu__item menu__item_selected">
+        <span>Sports</span>
         <Submenu />
       </div>
-      <div className="menu__item"><span>Brands</span></div>
-      <div className="menu__item"><span>Micoach</span></div>
+      <div className="menu__item">
+        <span>Brands</span>
+      </div>
+      <div className="menu__item">
+        <span>Micoach</span>
+      </div>
     </nav>
   )
 };
