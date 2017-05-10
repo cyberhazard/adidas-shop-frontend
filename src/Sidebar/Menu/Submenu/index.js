5d02ed0982a0d7f3785da70ee8f8fc0fbd6dbb3b
cyber <cyber>
2017-05-10 15:40:11 +0300

    menu

--- src/Sidebar/Menu/Submenu/index.js
+++ src/Sidebar/Menu/Submenu/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
-const Submenu = () => {
-  return (
-    <nav className="menu__submenu">
-      <div className="menu__subitem">
-        <a className="menu__link menu__link_active" href="#">Shoes</a>
-      </div>
-      <div className="menu__subitem">
-        <a className="menu__link" href="#">Clothing</a>
-      </div>
-      <div className="menu__subitem">
-        <a className="menu__link" href="#">Accesories</a>
-      </div>
-    </nav>
-  )
-};
+class Submenu extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { menu: props.menu }
+  }
+  handleClick(e,index){
+    e.stopPropagation();
+    let menu = this.state.menu.map((item,i)=>(item.active=index===i,item))
+    this.setState({menu})
+  }
+  render(){
+    return (
+      <nav className="menu__submenu">
+        {
+          this.state.menu.map((item,index)=>(
+            <div className="menu__subitem"
+                 onClick={e=>this.handleClick(e,index)}
+                 key={index}
+            >
+              <a className={`menu__link ${item.active? 'menu__link_active' : '' }`}
+                 href={item.link}
+              >{item.label}</a>
+            </div>
+          ))
+        }
+      </nav>
+    )
+  }
+}
+
 
 export default Submenu
--- src/Sidebar/Menu/index.js
+++ src/Sidebar/Menu/index.js
@@ -1,21 +1,103 @@
 import React from 'react';
 import Submenu from './Submenu';
 
-const Menu = () => {
-  return (
-    <nav className="menu">
-      <div className="menu__item menu__item_selected">
-        <span>Sports</span>
-        <Submenu />
-      </div>
-      <div className="menu__item">
-        <span>Brands</span>
-      </div>
-      <div className="menu__item">
-        <span>Micoach</span>
-      </div>
-    </nav>
-  )
-};
+const menu = [
+  {
+    label: 'Sports',
+    active: true,
+    submenu: [
+      {
+        label: 'Shoes',
+        active: true,
+        link: '#'
+      },
+      {
+        label: 'Clothing',
+        active: false,
+        link: '#'
+      },
+      {
+        label: 'Accesories',
+        active: false,
+        link: '#'
+      },
+    ]
+  },
+  {
+    label: 'Brands',
+    active: false,
+    submenu: [
+      {
+        label: 'Shoes',
+        active: true,
+        link: '#'
+      },
+      {
+        label: 'Clothing',
+        active: false,
+        link: '#'
+      },
+      {
+        label: 'Accesories',
+        active: false,
+        link: '#'
+      },
+    ]
+  },
+  {
+    label: 'Micoach',
+    active: false,
+    submenu: [
+      {
+        label: 'Shoes',
+        active: true,
+        link: '#'
+      },
+      {
+        label: 'Clothing',
+        active: false,
+        link: '#'
+      },
+      {
+        label: 'Accesories',
+        active: false,
+        link: '#'
+      },
+    ]
+  },
+]
+
+
+class Menu extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { menu }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(index){
+    let menu = this.state.menu.map((item,i)=>(item.active=index===i,item))
+    this.setState({menu})
+  }
+  render(){
+    return (
+      <nav className="menu">
+        {
+          this.state.menu.map((item,index)=>(
+            <div className={`menu__item ${item.active? 'menu__item_selected' : '' }`}
+                 onClick={e=>this.handleClick(index)}
+                 key={index} >
+              <span>{item.label}</span>
+              {
+                item.active && <Submenu key={index} menu={item.submenu} />
+              }
+            </div>
+          ))
+        }
+      </nav>
+    )
+  }
+}
+
+
 
 export default Menu
