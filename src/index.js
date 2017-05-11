d3552536d68d91348ee674ff0134991b0556bee8
cyber <cyber>
2017-05-12 02:44:59 +0300

    move named import to top

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 import './assets/css/fonts.css';
 import './assets/css/simple-reset.css'
-import App from './App';
 
 ReactDOM.render(
   <Router>
