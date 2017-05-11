2485bb9351c43484404936f3cff16607b1ac5809
cyber <cyber>
2017-05-11 15:41:57 +0300

    init

--- src/List/Filter/FilterButton/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FilterButton = ({label, selected}) => {
-  return (
-    <button className={`filter__text ${selected? 'filter__text_selected' : '' }`}>{label}</button>
-  )
-}
-
-export default FilterButton;
