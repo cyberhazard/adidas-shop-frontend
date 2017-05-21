d631e5df890c508de0547cf331e9f5a312c419b2
cyber <cyber>
2017-05-21 13:45:53 +0300

    Sidebar menu

--- src/Sidebar/Menu/index.jsx
+++ src/Sidebar/Menu/index.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { StyledMenu, MenuItem, MainLink, SubLink } from './styled';
+import { StyledMenu, MenuItem as MenuItemStyled, MainLink, SubLink } from './styled';
+
+class MenuItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isActive: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({ isActive: !this.state.isActive });
+  }
+  render() {
+    return (
+      <MenuItemStyled onClick={this.handleClick}>
+        <MainLink to={this.props.to} isSelected={this.state.isActive}>{this.props.text}</MainLink>
+        {this.state.isActive && this.props.children}
+      </MenuItemStyled>
+    );
+  }
+}
 
 export default () => (
   <StyledMenu>
-    <MenuItem>
-      <MainLink to="/" isSelected>Sports</MainLink>
+    <MenuItem to="/" text="Sports">
       <nav>
         <SubLink to="/" isSelected>Shoes</SubLink>
         <SubLink to="/">Clothing</SubLink>
         <SubLink to="/">Accessories</SubLink>
       </nav>
     </MenuItem>
-    <MenuItem>
-      <MainLink to="/" >Brands</MainLink>
-    </MenuItem>
-    <MenuItem>
-      <MainLink to="/" >Micoach</MainLink>
-    </MenuItem>
+    <MenuItem to="/" text="Brands" />
+    <MenuItem to="/" text="Micoach" />
   </StyledMenu>
 );
