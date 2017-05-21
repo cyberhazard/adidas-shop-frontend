4218e4256045359e1367b676d294dbb0f1f3da2e
cyber <cyber>
2017-05-22 01:58:17 +0300

    Changed мenu operation scheme

--- src/Sidebar/Menu/index.jsx
+++ src/Sidebar/Menu/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { StyledMenu, SubLink } from './styled';
-import MenuItem from './MenuItem';
+import { StyledMenu, MenuItem, MainLink as MainLinkStyled, SubLink as SubLinkStyled } from './styled';
+
+const SubLink = ({ to, children }) =>
+  <SubLinkStyled to={to} activeClassName="is-selected">{children}</SubLinkStyled>;
+
+const MainLink = ({ to, children }) =>
+  <MainLinkStyled to={to} activeClassName="is-selected">{children}</MainLinkStyled>;
 
 export default () => (
   <StyledMenu>
-    <MenuItem to="/" title="Sports">
+    <MenuItem>
+      <MainLink to="/sports">Sports</MainLink>
       <nav>
-        <SubLink to="/" isSelected>Shoes</SubLink>
-        <SubLink to="/">Clothing</SubLink>
-        <SubLink to="/">Accessories</SubLink>
+        <SubLink to="/sports/shoes">Shoes</SubLink>
+        <SubLink to="/sports/clothing">Clothing</SubLink>
+        <SubLink to="/sports/accessories">Accessories</SubLink>
       </nav>
     </MenuItem>
-    <MenuItem to="/" title="Brands" />
-    <MenuItem to="/" title="Micoach" />
+    <MenuItem>
+      <MainLink to="/brands">Brands</MainLink>
+    </MenuItem>
+    <MenuItem>
+      <MainLink to="/micoach">Micoach</MainLink>
+    </MenuItem>
   </StyledMenu>
 );
