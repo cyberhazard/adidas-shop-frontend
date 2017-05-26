36be585317efecceab81256a0abbe7134b52c43a
cyber <cyber>
2017-05-26 19:21:17 +0300

    feat: added redirect from '/'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import Sidebar from './Sidebar';
 import List from './List';
@@ -14,6 +14,7 @@ const Wrapper = styled.div`
 export default () => (
   <Wrapper>
     <Sidebar />
+    <Route exact path="/" render={() => <Redirect to="/products/football/cleats" />} />
     <Route exact path="/products/:group/:type" component={List} />
     <Route path="/products/:group/:type/:id" component={Details} />
   </Wrapper>
