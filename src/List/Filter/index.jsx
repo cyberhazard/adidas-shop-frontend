d3ff3c28cabd087080170f93b752f9b0df3d88f1
cyber <cyber>
2017-06-04 23:23:30 +0300

    feat: added filter

--- src/List/Filter/index.jsx
+++ src/List/Filter/index.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import { Wrapper, Image, Button, Label, Sizes } from './styled';
 
-export default () => (
-  <Wrapper>
-    <Image src={require('./filter.png')} />
-    <div>
-      <Button isSelected>Man</Button>
-      <Button>Woman</Button>
-    </div>
-    <Sizes>
-      <Label>Size</Label>
-      <Button mini>36</Button>
-      <Button mini>37</Button>
-      <Button mini>38</Button>
-      <Button mini>39</Button>
-      <Button mini>40</Button>
-      <Button mini isSelected>41</Button>
-      <Button mini>42</Button>
-    </Sizes>
-  </Wrapper>
-);
+export default class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedIndex: 0 };
+    this.selectSize = this.selectSize.bind(this);
+  }
+
+  selectSize(size, selectedIndex) {
+    this.props.onClick(size);
+    this.setState({ selectedIndex });
+  }
+
+  render() {
+    const sizes = ['All', ...this.props.sizes];
+    return (
+      <Wrapper>
+        <Image src={require('./filter.png')} />
+        <div>
+          <Button isSelected>Man</Button>
+          <Button>Woman</Button>
+        </div>
+        <Sizes>
+          <Label>Size</Label>
+          {
+            sizes.map((size, index) =>
+              (<Button
+                mini
+                isSelected={index === this.state.selectedIndex}
+                onClick={() => this.selectSize(size, index)}
+              >{ size }</Button>))
+          }
+        </Sizes>
+      </Wrapper>
+    );
+  }
+}
--- src/List/index.jsx
+++ src/List/index.jsx
@@ -22,8 +22,9 @@ const Col = ({ children }) => <StyledCol xs={12} sm={6} md={4} lg={3}>{children}
 export default class List extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { products: [] };
+    this.state = { products: [], filtered: [] };
     this.load = this.load.bind(this);
+    this.selectSize = this.selectSize.bind(this);
   }
 
   componentDidMount() {
@@ -39,17 +40,31 @@ export default class List extends React.Component {
     const FETCH_URL = `https://erodionov-adidas-fake-api.now.sh/v1/products/${group}/${type}`;
     fetch(FETCH_URL)
       .then(r => r.json())
-      .then(({ items }) => this.setState({ products: items }));
+      .then(({ items }) => this.setState({ products: items, filtered: items }));
+  }
+
+  selectSize(selectedSize) {
+    const filtered = selectedSize === 'All'
+      ? this.state.products
+      : this.state.products.filter(product => product.sizes.includes(selectedSize));
+    this.setState({ filtered });
   }
 
   render() {
+    const sizes = Array.from(new Set(this.state.products
+      // eslint-disable-next-line
+      .reduce((allSizes, obj) => (allSizes.push(...obj.sizes), allSizes), []),
+    ));
     return (
       <Wrapper>
-        <Filter />
+        <Filter
+          sizes={sizes}
+          onClick={this.selectSize}
+        />
         <Grid fluid>
           <Row>
             {
-              this.state.products.map((product) => {
+              this.state.filtered.map((product) => {
                 const { id, fileName } = product.images[0];
                 return (
                   <Col key={product.id}>
