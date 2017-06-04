d25076bd2b403b272267715d5c9d990b1f6150b0
cyber <cyber>
2017-06-05 00:11:10 +0300

    feat: added API

--- src/Details/index.jsx
+++ src/Details/index.jsx
@@ -9,8 +9,25 @@ import Description from './Description';
 export default class Details extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { color: '' };
+    this.state = { color: '', item: {} };
     this.changeColor = this.changeColor.bind(this);
+    this.load = this.load.bind(this);
+  }
+
+  componentDidMount() {
+    this.load(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.load(nextProps);
+  }
+
+  load(props) {
+    const { group, type, id } = props.match.params;
+    const FETCH_URL = `https://erodionov-adidas-fake-api.now.sh/v1/products/${group}/${type}/${id}`;
+    fetch(FETCH_URL)
+      .then(r => r.json())
+      .then(item => this.setState({ item }));
   }
 
   changeColor(color) {
